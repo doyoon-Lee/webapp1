e0aeec5e0d7af233aeba59367e288a8284157a35
Doyoon-Lee <yjlee9506@naver.com>
2021-01-14 09:16:18 +0900

    필드2와 3 위치변경

--- src/main/java/com/mycompany/webapp/controller/TestController.java
+++ src/main/java/com/mycompany/webapp/controller/TestController.java
@@ -3,8 +3,8 @@ package com.mycompany.webapp.controller;
 public class TestController {
 	//Field
 	private String field1;
-	private String field3;
 	private String field2;
+	private String field3;
 	//Constructor
 	//Method
 }

b23f2af490ee7fd159d06504070e4a7fd73efe06
Doyoon-Lee <yjlee9506@naver.com>
2021-01-19 16:49:45 +0900

    ojdbc6추가 pom.xml에도 추가

--- src/main/java/com/mycompany/webapp/controller/Ch13Controller.java
+++ src/main/java/com/mycompany/webapp/controller/Ch13Controller.java
@@ -4,7 +4,7 @@ import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
